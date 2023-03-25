11fd22fbb3d421ad4d8fe991815d98530c80e545
Chase Austin <chase7867@gmail.com>
2023-03-24 23:22:13 -0500

    added additional check for audio file exists. added print statement.

--- reddit_youtube/utils/audio.py
+++ reddit_youtube/utils/audio.py
@@ -3,9 +3,14 @@ import os
 def file_exists(audio, s3_files):
     # clip segment
     if ".wav" in audio:
+        print(f"{audio} exists locally")
+        if os.path.isfile(audio):
+            return True
+        print(f"{audio} exists on s3")
         if audio in s3_files:
             s3.download(audio)
             return True
+
     # full_clip
     else:
         # create local output dir if path
@@ -13,5 +18,6 @@ def file_exists(audio, s3_files):
             os.makedirs(audio)
         # if clip file has been generated continue
         if f"{audio}.wav" in s3_files:
+            print(f"{audio}.wav exists on s3")
             return True
     return False
